3803f3cdf9bf8b1006074803238dc70320b87d30
aduratovic <aduratovic22@gmail.com>
2022-06-25 22:37:23 -0400

    minor update

---
+++ script.js
@@ -0,0 +1,15 @@
+const displayStatus = document.querySelector('.status');
+
+const winningMessage = () => {
+    return `Player ${currentPlayer} has won!`;
+}
+const currentPlayerTurn = () => {
+    return `It's ${currentPlayer}'s turn`;
+}
+const tie = () => { 
+    return `Tie!`;
+}
+
+
+
+
